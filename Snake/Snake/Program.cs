faef7e7db14f43400cbea03534974c283d812c75
Julia666 <zazeralie@yandex.ru>
2019-03-27 06:01:51 +0300

    создала класс List из Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,14 +12,24 @@ namespace Snake
         {
 
             Point p1 = new Point(1, 3, '*');
-
             p1.Draw();
 
             Point p2 = new Point(4, 5,'#');
-
             p2.Draw();
 
+            Point p3 = new Point(6, 8, '@');
+            p3.Draw();
+
+            Point p4 = new Point(12, 14, '&');
+            p4.Draw();
+
             Console.ReadLine();
+
+            List<Point> pList = new List<Point>();
+            pList.Add( p1 );
+            pList.Add( p2 );
+            pList.Add( p3 );
+            pList.Add( p4 );
         }
     }
 }
